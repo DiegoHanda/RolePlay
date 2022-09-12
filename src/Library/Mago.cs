462bfec06aa0f9030a45cdaa2eb363c2d04fa8cf
DiegoHanda <diego.handalian@correo.ucu.edu.uy>
2022-09-12 20:29:54 -0300

    Inicio Clases

---
+++ src/Library/Mago.cs
@@ -0,0 +1,15 @@
+namespace Mago;
+public class Mago
+{
+    public string Nombre {get; set;}
+    public float Salud {get; set;}
+    public int Nivel {get; set;}
+
+    public Mago (string nombre, float salud, string item)
+    {   
+        this.Nombre = nombre;
+        this.Salud = salud;
+        this.Nivel = 1;
+    }
+
+}
