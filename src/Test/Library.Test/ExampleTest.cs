940682d3a6bb25c9d3158dae6c60677f6b6cea76
braimor <briangmsosa@gmail.com>
2022-10-14 21:23:35 -0300

    test 2

--- src/Test/Library.Test/ExampleTest.cs
+++ src/Test/Library.Test/ExampleTest.cs
@@ -15,5 +15,16 @@ namespace Test.Library
            int saludEsperadaEnano = 1450;
             Assert.AreEqual(saludEsperadaEnano,saludActualEnano);
         }
+        
+        [Test]
+        public void chequearAtacarYRecibirDaño()
+        {
+           Enano enano1 = new Enano("enano1");
+           Mago mago1 = new Mago ("mago1");
+           mago1.Atacar(enano1);
+           int saludActualEnano = enano1.Salud;
+           int saludEsperadaEnano = 1290;
+            Assert.AreEqual(saludEsperadaEnano,saludActualEnano);
+        }
     }
 }
